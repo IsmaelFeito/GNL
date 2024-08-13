d3cfbf62efd21ace81af2bad9251048cd39ffe0d
IsmaelFeito <168192289+IsmaelFeito@users.noreply.github.com>
2024-08-13 22:10:03 +0200

    Add files via upload
    gnl final con lists enlazadas

---
+++ GNL/get_next_line.c
@@ -0,0 +1,152 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ifeito-m <ifeito-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/29 18:37:20 by ifeito-m          #+#    #+#             */
+/*   Updated: 2024/08/07 13:58:53 by ifeito-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void	clean_list(t_list **list)
+{
+	t_list	*last_node;
+	t_list	*clean_node;
+	int		i;
+	int		j;
+	char	*buf;
+
+	buf = malloc(BUFFER_SIZE +1);
+	clean_node = malloc(sizeof(t_list));
+	if (NULL == buf || NULL == clean_node)
+		return ;
+	last_node = find_last_node(*list);
+	i = 0;
+	j = 0;
+	while (last_node->str_buf[i] && last_node->str_buf[i] != '\n')
+		++i;
+	while (last_node->str_buf[i] && last_node->str_buf[++i])
+		buf[j++] = last_node->str_buf[i];
+	buf[j] = '\0';
+	clean_node->str_buf = buf;
+	clean_node->next = NULL;
+	free_nodes(list, clean_node, buf);
+}
+
+char	*get_line(t_list *list)
+{
+	int		str_len;
+	char	*next_str;
+
+	if (list == NULL)
+		return (NULL);
+	str_len = len_to_newline(list);
+	next_str = malloc(str_len +1);
+	if (next_str == NULL)
+		return (NULL);
+	copy_str(list, next_str);
+	return (next_str);
+}
+
+void	link_new_node(t_list **list, char *buf)
+{
+	t_list	*new_node;
+	t_list	*last_node;
+
+	last_node = find_last_node(*list);
+	new_node = malloc(sizeof(t_list));
+	if (new_node == NULL)
+		return ;
+	if (last_node == NULL)
+		*list = new_node;
+	else
+		last_node->next = new_node;
+	new_node->str_buf = buf;
+	new_node->next = NULL;
+}
+
+int	create_list(t_list **list, int fd)
+{
+	int		char_read;
+	char	*buf;
+
+	while (!find_newline(*list))
+	{
+		buf = malloc(BUFFER_SIZE + 1);
+		if (NULL == buf)
+			return (1);
+		char_read = read(fd, buf, BUFFER_SIZE);
+		if (char_read < 0)
+		{
+			free(buf);
+			return (1);
+		}
+		if (char_read == 0)
+		{
+			free(buf);
+			return (0);
+		}
+		buf[char_read] = '\0';
+		link_new_node(list, buf);
+	}
+	return (0);
+}
+
+char	*get_next_line(int fd)
+{
+	static t_list	*list = NULL;
+	char			*next_line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (create_list(&list, fd) == 1)
+		return (NULL);
+	if (list == NULL)
+		return (NULL);
+	next_line = get_line(list);
+	clean_list(&list);
+	return (next_line);
+}
+
+int main()
+{
+    char    *line1;
+	char	*line2;
+    int     fd1;
+	int		fd2;
+
+    fd1 = open("prueba3.txt", O_RDONLY);
+	fd2 = open("prueba2.txt", O_RDONLY);
+	if (fd1 == -1 && fd2 == -1)
+	{
+		close(fd2);
+		close(fd1);
+		return (0);
+	}
+	line1 = "";
+	line2 = "";
+    while (line1 != NULL || line2 != NULL)
+    {
+        line1 = get_next_line(fd1);
+		line2 = get_next_line(fd2);
+		if (!line1 && !line2)
+		{
+			free (line1);
+			free (line2);
+			break;
+		}
+        printf("%s", line1);
+		printf("%s", line2);
+		free(line1);
+		free(line2);
+    }
+	free(line1);
+	close(fd1);
+    return (0);
+}
---
+++ GNL/get_next_line.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ifeito-m <ifeito-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/29 15:54:21 by ifeito-m          #+#    #+#             */
+/*   Updated: 2024/07/31 14:20:32 by ifeito-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	char			*str_buf;
+	struct s_list	*next;
+}				t_list;
+
+int		find_newline(t_list *list);
+t_list	*find_last_node(t_list *list);
+char	*get_line(t_list *list);
+void	copy_str(t_list *list, char *str);
+int		len_to_newline(t_list *list);
+void	clean_list(t_list **list);
+char	*get_next_line(int fd);
+void	free_nodes(t_list **list, t_list *clean_node, char *buf);
+int		create_list(t_list **list, int fd);
+
+#endif
